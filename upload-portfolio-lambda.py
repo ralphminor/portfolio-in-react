37156c811d06da42370b79578b33f608bb1906de
ralphminor <ralphminor@users.noreply.github.com>
2017-09-07 17:36:07 -0600

    Updated portfolio upload script to AWS lambda function.

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -3,18 +3,23 @@ import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
 
-portfolio_bucket = s3.Bucket('portfolio.ralphminor.com')
-build_bucket = s3.Bucket('portfoliobuild.ralphminor.com')
+    portfolio_bucket = s3.Bucket('portfolio.ralphminor.com')
+    build_bucket = s3.Bucket('portfoliobuild.ralphminor.com')
 
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+     for nm in myzip.namelist():
         obj = myzip.open(nm)
         portfolio_bucket.upload_fileobj(obj, nm,
-          ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]}
+            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]}
         )
         portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    print "Job done!"
+
+    return 'Hello from Lambda'
